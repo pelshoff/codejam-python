8f5fb78b489e4ae37573076625fe7f1cda5d0997
Pim Elshoff <pim@pelshoff.com>
2012-04-01 23:55:35 +0200

    Small refactoring

--- reversewords.py
+++ reversewords.py
@@ -2,14 +2,16 @@ import string
 import sys
 from wordsreverser import WordsReverser
 
-reverser = WordsReverser()
-
 def parseInput(inputString):
     input = string.split(inputString.strip(), "\n")
     return input[1:int(input[0])+1]
 
+def reverseInput(inFile, outFile):
+    reverser = WordsReverser()
+    sentences = parseInput(open(inFile, 'r').read())
+    output = reverser.formatOutput(reverser.reverseSet(sentences)).strip()
+    open(outFile, 'w').write(output)
+
 inFile = sys.argv[1]
 outFile = inFile.replace('.in', '.out')
-sentences = parseInput(open(inFile, 'r').read())
-output = reverser.formatOutput(reverser.reverseSet(sentences)).strip()
-open(outFile, 'w').write(output)
+reverseInput(inFile, outFile)
